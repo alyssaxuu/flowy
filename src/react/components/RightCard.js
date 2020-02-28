3d408f7479c1dca23b8a94dcf9b689c81aee9380
Joel Van Horn <joel@joelvanhorn.com>
2020-02-28 04:13:19 -0500

    Fix right card shadow and remove wrapper

--- src/react/components/RightCard.js
+++ src/react/components/RightCard.js
@@ -4,40 +4,38 @@ import images from '../images'
 export default function({ open, onClose }) {
   return (
     <>
-      <div id="propwrap" className={open ? 'itson' : ''}>
-        <div id="properties" className={open ? 'expanded' : ''}>
-          <div id="close" onClick={onClose}>
-            <img src={images.close} />
+      <div id="properties" className={open ? 'expanded' : ''}>
+        <div id="close" onClick={onClose}>
+          <img src={images.close} />
+        </div>
+        <p id="header2">Properties</p>
+        <div id="propswitch">
+          <div id="dataprop">Data</div>
+          <div id="alertprop">Alerts</div>
+          <div id="logsprop">Logs</div>
+        </div>
+        <div id="proplist">
+          <p className="inputlabel">Select database</p>
+          <div className="dropme">
+            Database 1 <img src={images.dropdown} />
           </div>
-          <p id="header2">Properties</p>
-          <div id="propswitch">
-            <div id="dataprop">Data</div>
-            <div id="alertprop">Alerts</div>
-            <div id="logsprop">Logs</div>
+          <p className="inputlabel">Check properties</p>
+          <div className="dropme">
+            All
+            <img src={images.dropdown} />
           </div>
-          <div id="proplist">
-            <p className="inputlabel">Select database</p>
-            <div className="dropme">
-              Database 1 <img src={images.dropdown} />
-            </div>
-            <p className="inputlabel">Check properties</p>
-            <div className="dropme">
-              All
-              <img src={images.dropdown} />
-            </div>
-            <div className="checkus">
-              <img src={images.checkon} />
-              <p>Log on successful performance</p>
-            </div>
-            <div className="checkus">
-              <img src={images.checkoff} />
-              <p>Give priority to this block</p>
-            </div>
+          <div className="checkus">
+            <img src={images.checkon} />
+            <p>Log on successful performance</p>
           </div>
-          <div id="divisionthing"></div>
-          <div id="removeblock" onClick={() => flowy.deleteBlocks()}>
-            Delete blocks
+          <div className="checkus">
+            <img src={images.checkoff} />
+            <p>Give priority to this block</p>
           </div>
+        </div>
+        <div id="divisionthing"></div>
+        <div id="removeblock" onClick={() => flowy.deleteBlocks()}>
+          Delete blocks
         </div>
       </div>
     </>
