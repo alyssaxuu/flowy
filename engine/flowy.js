2bedfba1095e39a6e841e3bbd6a7df416f2ee7a5
Alyssa X <hi@alyssax.com>
2020-07-04 14:57:14 +0200

    IE11 .closest workaround

--- engine/flowy.js
+++ engine/flowy.js
@@ -21,6 +21,20 @@ var flowy = function(canvas, grab, release, snapping, rearrange, spacing_x, spac
     if (!spacing_y) {
         spacing_y = 80;
     }
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                              Element.prototype.webkitMatchesSelector;
+    }
+    if (!Element.prototype.closest) {
+      Element.prototype.closest = function(s) {
+        var el = this;
+        do {
+          if (Element.prototype.matches.call(el, s)) return el;
+          el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+      };
+    }
     var loaded = false;
     flowy.load = function() {
         if (!loaded)
